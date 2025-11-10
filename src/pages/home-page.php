8c45d74a5a2fd4a22fd4561a03d1bee50dad49b2
Dario Stromajer <115354821+DarioLoll@users.noreply.github.com>
2025-11-10 20:00:03 +0100

    Extract homepage into separate file, implement simple router in index.php

---
+++ src/pages/home-page.php
@@ -0,0 +1,53 @@
+<?php
+require_once __DIR__ . '/../domain/Note.php';
+use Domain\Note;
+
+$notes = [
+    new Note(
+        id: 1,
+        title: "PHP Advanced abcdrefef",
+        description: "Advanced PHP concepts and examples.",
+        date: "2025-10-23",
+        user: "Dario",
+        subject: "WEB",
+        grade: 1
+    ),
+    new Note(
+        id: 2,
+        title: "Betriebssysteme",
+        description: "Einführung Ubuntu, VM installieren, etc.",
+        date: "2025-10-23",
+        user: "Ali",
+        subject: "INFRA",
+        grade: 1
+    ),
+    new Note(
+        id: 3,
+        title: "Function pointer",
+        description: "Beispielapp mit hierarchischen Menüs",
+        date: "2025-10-23",
+        user: "Dario",
+        subject: "PROZ",
+        grade: 1
+    ),
+    new Note(
+        id: 4,
+        title: "SQL",
+        description: null,
+        date: "2025-10-22",
+        user: "Ali",
+        subject: "DBMG",
+        grade: 1
+    ),
+];
+?>
+
+<div class="container mt-3">
+    <div class="row">
+        <?php foreach ($notes as $note): ?>
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3 my-3 d-flex justify-content-center">
+                <?php require '../components/note_card.php'; ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
--- src/public/index.php
+++ src/public/index.php
@@ -1,47 +1,3 @@
-<?php
-require_once __DIR__ . '/../domain/Note.php';
-use Domain\Note;
-
-$notes = [
-    new Note(
-        id: 1,
-        title: "PHP Advanced abcdrefef",
-        description: "Advanced PHP concepts and examples.",
-        date: "2025-10-23",
-        user: "Dario",
-        subject: "WEB",
-        grade: 1
-    ),
-    new Note(
-        id: 2,
-        title: "Betriebssysteme",
-        description: "Einführung Ubuntu, VM installieren, etc.",
-        date: "2025-10-23",
-        user: "Ali",
-        subject: "INFRA",
-        grade: 1
-    ),
-    new Note(
-        id: 3,
-        title: "Function pointer",
-        description: "Beispielapp mit hierarchischen Menüs",
-        date: "2025-10-23",
-        user: "Dario",
-        subject: "PROZ",
-        grade: 1
-    ),
-    new Note(
-        id: 4,
-        title: "SQL",
-        description: null,
-        date: "2025-10-22",
-        user: "Ali",
-        subject: "DBMG",
-        grade: 1
-    ),
-];
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -55,14 +11,24 @@ $notes = [
 <body data-bs-theme="dark">
     <?php include_once '../components/navbar.php'; ?>
 
-    <div class="container mt-3">
-        <div class="row">
-            <?php foreach ($notes as $note): ?>
-                <div class="col-12 col-md-6 col-lg-4 col-xl-3 my-3 d-flex justify-content-center">
-                    <?php require '../components/note_card.php'; ?>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </div>
+    <?php
+
+    $url = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+
+    switch($url) {
+        case 'home':
+        case '': // Default page (if no URL is specified)
+            require __DIR__ . '/../pages/home-page.php';
+            break;
+        case 'login':
+            require __DIR__ . '/../pages/login-page.php';
+            break;
+        default:
+            http_response_code(404);
+            echo "Page not found";
+            break;
+    }
+    ?>
+
 </body>
 </html>
