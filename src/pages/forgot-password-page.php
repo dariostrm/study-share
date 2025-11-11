fe60bc4c3f81f8d690732d20ff9df16111b62f60
Dario Stromajer <115354821+DarioLoll@users.noreply.github.com>
2025-11-11 21:49:02 +0100

    Tweak login/signup windows for desktop

--- src/pages/forgot-password-page.php
+++ src/pages/forgot-password-page.php
@@ -2,32 +2,34 @@
 
 ?>
 
-<div class="d-flex flex-column justify-content-center align-items-center vh-100">
-    <div class="card bg-dark-subtle px-3 py-4 rounded-4">
-        <div class="card-body d-flex flex-column align-items-center gap-1">
-            <h2>Reset Password</h2>
-            <p class="text-muted">
-                Back to <a href="/login">Login</a>
-            </p>
-            <form action="./sign-up-page.php" method="post" class="d-flex flex-column gap-3">
-                <div class="form-floating">
-                    <input type="email" class="form-control" id="email"
-                           name="email" placeholder="E-Mail" required>
-                    <label for="email">E-Mail</label>
-                </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" placeholder="New Password"
-                           id="password" name="password" required>
-                    <label for="password">New Password</label>
-                </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" placeholder="Confirm Password"
-                           id="confirm" name="confirm" required>
-                    <label for="confirm">Confirm Password</label>
-                </div>
+<div class="container h-100">
+    <div class="row h-100 d-flex flex-column justify-content-center align-items-center">
+        <div class="card col-10 col-md-8 col-lg-6 col-xl-4 bg-dark-subtle p-4 rounded-4">
+            <div class="card-body d-flex flex-column align-items-center gap-1 w-100">
+                <h2>Reset Password</h2>
+                <p class="text-muted">
+                    Back to <a href="/login">Login</a>
+                </p>
+                <form action="./sign-up-page.php" method="post" class="d-flex flex-column gap-3 w-100">
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="email"
+                               name="email" placeholder="E-Mail" required>
+                        <label for="email">E-Mail</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" placeholder="New Password"
+                               id="password" name="password" required>
+                        <label for="password">New Password</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" placeholder="Confirm Password"
+                               id="confirm" name="confirm" required>
+                        <label for="confirm">Confirm Password</label>
+                    </div>
 
-                <button type="submit" class="btn btn-primary mt-4">Continue</button>
-            </form>
+                    <button type="submit" class="btn btn-primary mt-4">Continue</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
--- src/pages/login-page.php
+++ src/pages/login-page.php
@@ -10,28 +10,30 @@
     }
 ?>
 
-<div class="d-flex flex-column justify-content-center align-items-center vh-100">
-    <div class="card bg-dark-subtle px-3 py-4 rounded-4">
-        <div class="card-body d-flex flex-column align-items-center gap-1">
-            <h2>Login</h2>
-            <p class="text-muted">
-                First time here? <a href="/sign-up">Sign Up</a>
-            </p>
-            <form action="<?php echo $_SERVER['REQUEST_URI']?>" method="post" class="d-flex flex-column gap-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="username"
-                           name="username" placeholder="username" required>
-                    <label for="username">Username</label>
-                </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" placeholder="Password"
-                           id="password" name="password" required>
-                    <label for="password">Password</label>
-                </div>
-                <a href="/forgot-password">Forgot Password?</a>
+<div class="container h-100">
+    <div class="row h-100 d-flex flex-column justify-content-center align-items-center">
+        <div class="card col-10 col-md-8 col-lg-6 col-xl-4 bg-dark-subtle p-4 rounded-4">
+            <div class="card-body d-flex flex-column align-items-center gap-1 w-100">
+                <h2>Login</h2>
+                <p class="text-muted">
+                    First time here? <a href="/sign-up">Sign Up</a>
+                </p>
+                <form action="<?php echo $_SERVER['REQUEST_URI']?>" method="post" class="d-flex flex-column gap-3 w-100">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="username"
+                               name="username" placeholder="username" required>
+                        <label for="username">Username</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" placeholder="Password"
+                               id="password" name="password" required>
+                        <label for="password">Password</label>
+                    </div>
+                    <a href="/forgot-password">Forgot Password?</a>
 
-                <button type="submit" class="btn btn-primary mt-4">Login</button>
-            </form>
+                    <button type="submit" class="btn btn-primary mt-4">Login</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
